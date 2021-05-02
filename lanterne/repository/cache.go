436ffb35d3feb6beb645518be4cfa50473a86a80
hiroki <hiroki@users.noreply.github.com>
2021-05-02 20:44:48 +0900

    add: expand

--- lanterne/repository/cache.go
+++ lanterne/repository/cache.go
@@ -42,18 +42,56 @@ func (c *CacheGraphRepository) LoadAdjacent(ctx context.Context, query model.Adj
 }
 
 func (c *CacheGraphRepository) DumpVertex(ctx context.Context, vertex model.Vertex) error {
-	c.vertices.Set(vertex.Digest(), vertex)
+	go c.vertices.Set(vertex.Digest(), vertex)
 	return nil
 }
 
 func (c *CacheGraphRepository) DumpEdge(ctx context.Context, edge model.Edge) error {
-	if err := c.DumpVertex(ctx, edge.Tail); err != nil {
-		return err
-	}
-	if err := c.DumpVertex(ctx, edge.Head); err != nil {
-		return err
-	}
-	c.edges.Set(edge.Tail.Digest(), edge.Head.Digest(), edge.Weight)
+	go c.vertices.Set(edge.Tail.Digest(), edge.Tail)
+	go c.vertices.Set(edge.Head.Digest(), edge.Head)
+	go c.edges.Set(edge.Tail.Digest(), edge.Head.Digest(), edge.Weight)
 
 	return nil
+}
+
+func (c *CacheGraphRepository) getAdjacentGraph(tail model.Vertex) model.Graph {
+	result := model.NewGraph()
+	result.Vertices[tail.Digest()] = tail
+	heads, found := c.edges.GetAdjacent(tail.Digest())
+	if !found {
+		return result
+	}
+	for headDigest, weight := range heads {
+		head, found := c.vertices.Get(headDigest)
+		if found {
+			_, ok := result.Edges[tail.Digest()]
+			if !ok {
+				result.Edges[tail.Digest()] = make(map[string]float32)
+			}
+			result.Vertices[head.Digest()] = head
+			result.Edges[tail.Digest()][head.Digest()] = weight
+		}
+	}
+
+	return result
+}
+
+func union(graphArray []model.Graph) model.Graph {
+	result := model.NewGraph()
+	for _, graph := range graphArray {
+		for k, v := range graph.Vertices {
+			result.Vertices[k] = v
+		}
+		for tail, headMap := range result.Edges {
+			for head, weight := range headMap {
+				_, ok := result.Edges[tail]
+				if !ok {
+					result.Edges[tail] = make(map[string]float32)
+				}
+				result.Edges[tail][head] = weight
+			}
+		}
+	}
+
+	return result
 }
