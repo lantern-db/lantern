0e658075e9e615ffcfc131c278312aa0a32c2bbf
hiroki <hiroki@users.noreply.github.com>
2021-05-04 20:46:51 +0900

    add: factory

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	pb "github.com/piroyoung/lanterne/grpc"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"math"
 	"strconv"
 	"time"
@@ -64,13 +65,17 @@ func (c *LanterneClient) DumpEdge(ctx context.Context, tail string, head string,
 	return nil
 }
 
-func (c *LanterneClient) DumpVertex(ctx context.Context, key string) error {
-	response, err := c.client.DumpVertex(ctx, &pb.Vertex{Key: key})
+func (c *LanterneClient) DumpVertex(ctx context.Context, key string, value interface{}) error {
+	vertex, err := newVertex(key, value)
+	if err != nil {
+		return err
+	}
+	response, err := c.client.DumpVertex(ctx, vertex)
 	if err != nil {
 		return err
 	}
 	if response.Status != pb.Status_OK {
-		return errors.New("dump vertex error")
+		return errors.New("dump vertex error. status: " + pb.Status_OK.String())
 	}
 	return nil
 }
@@ -87,7 +92,51 @@ func (c *LanterneClient) Illuminate(ctx context.Context, seed string, step uint3
 		return nil, err
 	}
 	if response.Status != pb.Status_OK {
-		return nil, errors.New("illuminate error")
+		return nil, errors.New("illuminate error. status: " + response.Status.String())
 	}
 	return response.Graph, nil
+}
+
+func newVertex(key string, value interface{}) (*pb.Vertex, error) {
+	vertex := &pb.Vertex{
+		Key: key,
+	}
+	switch v := value.(type) {
+	case float64:
+		vertex.Value = &pb.Vertex_Float64{Float64: v}
+
+	case float32:
+		vertex.Value = &pb.Vertex_Float32{Float32: v}
+
+	case int32:
+		vertex.Value = &pb.Vertex_Int32{Int32: v}
+
+	case int64:
+		vertex.Value = &pb.Vertex_Int64{Int64: v}
+
+	case uint32:
+		vertex.Value = &pb.Vertex_Uint32{Uint32: v}
+
+	case uint64:
+		vertex.Value = &pb.Vertex_Uint64{Uint64: v}
+
+	case bool:
+		vertex.Value = &pb.Vertex_Bool{Bool: v}
+
+	case string:
+		vertex.Value = &pb.Vertex_String_{String_: v}
+
+	case []byte:
+		vertex.Value = &pb.Vertex_Bytes{Bytes: v}
+
+	case time.Time:
+		vertex.Value = &pb.Vertex_Timestamp{Timestamp: timestamppb.New(v)}
+
+	case nil:
+		vertex.Value = &pb.Vertex_Bool{Bool: true}
+
+	default:
+		return nil, errors.New("type mismatch")
+	}
+	return vertex, nil
 }
