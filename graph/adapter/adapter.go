f1065e2188b273a310c176e4c29148212cd7a763
hiroki <hiroki@users.noreply.github.com>
2022-01-31 00:00:47 +0900

    revise: message structure

--- graph/adapter/adapter.go
+++ graph/adapter/adapter.go
@@ -1,45 +1,50 @@
 package adapter
 
 import (
-	"github.com/lantern-db/lantern/graph/model"
+	. "github.com/lantern-db/lantern/graph/model"
 	pb "github.com/lantern-db/lantern/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"time"
 )
 
-func LanternQuery(request *pb.IlluminateRequest) model.LoadQuery {
-	return model.LoadQuery{
-		Seed:      &model.ProtoVertex{Message: request.Seed},
+func LanternQuery(request *pb.IlluminateRequest) LoadQuery {
+	return LoadQuery{
+		Seed:      Key(request.Seed),
 		Step:      request.Step,
 		MinWeight: request.MinWeight,
 		MaxWeight: request.MaxWeight,
 	}
 }
 
-func LanternVertex(vertex *pb.Vertex) model.Vertex {
-	return &model.ProtoVertex{
-		Message: vertex,
+func LanternVertex(vertex *pb.Vertex) Vertex {
+	return Vertex{
+		Key:        Key(vertex.Key),
+		Expiration: Expiration(vertex.Expiration.AsTime().Unix()),
+		Value:      vertex.Value,
 	}
 }
 
-func LanternEdge(edge *pb.Edge) model.Edge {
-	return model.Edge{
-		Tail:   LanternVertex(edge.Tail),
-		Head:   LanternVertex(edge.Head),
-		Weight: edge.Weight,
+func LanternEdge(edge *pb.Edge) Edge {
+	return Edge{
+		Tail:   Key(edge.Tail),
+		Head:   Key(edge.Head),
+		Weight: Weight(edge.Weight),
 	}
 }
 
-func ProtoGraph(graph model.Graph) *pb.Graph {
+func ProtoGraph(graph Graph) *pb.Graph {
 	g := pb.Graph{}
 	g.VertexMap = make(map[string]*pb.Vertex)
-	for _, value := range graph.VertexMap {
-		switch v := value.Value().(type) {
+	for _, vertex := range graph.VertexMap {
+		switch v := vertex.Value.(type) {
 		case *pb.Vertex:
-			g.VertexMap[value.Key()] = v
+			g.VertexMap[string(vertex.Key)] = v
 
 		default:
-			g.VertexMap[value.Key()] = &pb.Vertex{
-				Key:   value.Key(),
-				Value: &pb.Vertex_Nil{Nil: true},
+			g.VertexMap[string(vertex.Key)] = &pb.Vertex{
+				Key:        string(vertex.Key),
+				Value:      &pb.Vertex_Nil{Nil: true},
+				Expiration: timestamppb.New(time.Unix(int64(vertex.Expiration), 0)),
 			}
 		}
 	}
@@ -49,9 +54,9 @@ func ProtoGraph(graph model.Graph) *pb.Graph {
 		neighbor := pb.Neighbor{}
 		neighbor.WeightMap = make(map[string]float32)
 		for headKey, weight := range heads {
-			neighbor.WeightMap[headKey] = weight
+			neighbor.WeightMap[string(headKey)] = float32(weight)
 		}
-		g.NeighborMap[tailKey] = &neighbor
+		g.NeighborMap[string(tailKey)] = &neighbor
 	}
 	return &g
 }
